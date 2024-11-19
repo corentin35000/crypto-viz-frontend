1f66f2d74124110c6e8af31395d6fadf3486681f
Corentin Recanzone <corentindevpro@hotmail.com>
2024-11-20 00:38:13 +0100

    feat: add for test connection nats server

--- src-nuxt/pages/index.vue
+++ src-nuxt/pages/index.vue
@@ -1,3 +1,18 @@
 <template>
   <h1 class="flex">INDEX PAGE</h1>
 </template>
+
+<script lang="ts" setup>
+import { onMounted } from 'vue'
+import NatsService from '~~/src-core/services/nats_service'
+
+const natsService: NatsService = new NatsService()
+
+onMounted(async (): Promise<void> => {
+  try {
+    await natsService.connect()
+  } catch (error) {
+    console.error('Erreur de connexion à NATS:', error)
+  }
+})
+</script>
